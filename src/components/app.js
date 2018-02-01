c2390715ccd256da786b68af3839500d4ae7a24e
Shawn Levin <shawnlevin22@gmail.com>
2018-01-31 17:13:51 -0800

    finished functions

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import 'materialize-css/dist/css/materialize.min.css';
+import '../assets/css/app.css';
 import React from 'react';
 import Game from './game';
 
 const App = () => (
     <div className="container">
-        <h1 className="center-align z-depth-3 indigo-text darken-4" >Guessing Game</h1>
+        <h1 className="center-align z-depth-3 indigo-text darken-4" >GuessinG Game</h1>
         <Game />
     </div>
 );
